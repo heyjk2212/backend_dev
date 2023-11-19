172fb7bc69eefad940a0cfb73d0fb2d56f6ec8c9
heyjk2212 <jinakim2212@gmail.com>
2023-11-19 13:58:57 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ import express from "express";
 import UsersRouter from "./routes/users.router.js";
 import OrdersRouter from "./routes/orders.router.js";
 import GoodsRouter from "./routes/goods.router.js";
-import GoodsCommentsRouter from "./goods.router.js";
+import GoodsCommentsRouter from "./routes/goods.comments.router.js";
+import cookieParser from "cookie-parser";
+import errorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
+import logMiddleware from "./middlewares/log.middleware.js";
 
 const app = express();
 const router = express.Router();
 const PORT = 3000;
 
-app.use(express.json()); // body parser
+app.use(logMiddleware);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 router.get("/", (req, res) => {
   return res.status(200).json({ message: "성공!" });
@@ -22,8 +29,10 @@ app.use("/api", [
   GoodsCommentsRouter,
 ]);
 
+app.use(errorHandlingMiddleware);
+
 app.listen(PORT, () => {
-  console.log(PORT, `${PORT} 포트로 서버가 열렸어요!`);
+  console.log(PORT, `Server running on port ${PORT}`);
 });
 
 export default router;
