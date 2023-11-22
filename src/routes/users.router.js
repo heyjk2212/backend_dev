167dd38283b785561850ce33e9e4d1b212af4a12
heyjk2212 <jinakim2212@gmail.com>
2023-11-22 17:18:21 +0900

    Update login api

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -82,11 +82,9 @@ router.post("/login", async (req, res, next) => {
       secretKey
     );
 
-    // res.cookie("authorization", `Bearer ${token}`);
-
     return res
       .status(200)
-      .json({ message: "로그인에 성공하였습니다.", token: token });
+      .json({ message: "로그인에 성공하였습니다.", token: `Bearer ${token}` });
   } catch (err) {
     next(err);
   }
